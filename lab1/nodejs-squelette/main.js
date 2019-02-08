2ffd9394da782274ebb23d23a82e33f3264458e7
mjj94 <m.jammal94@gmail.com>
2019-02-08 14:01:48 +0100

    -a

--- lab1/nodejs-squelette/main.js
+++ lab1/nodejs-squelette/main.js
@@ -40,7 +40,6 @@ app.post('/api/run', (req, res, next) => {
 
 app.delete('/api/run/:run_id', (req, res, next) => {
   const run_ids = req.params.run_id.split(',').map(v => parseInt(v)).filter(v => !isNaN(v))
-  console.log(run_ids)
 run_ids.forEach(runId => {console.log(runId)
   	const recordKey = datastore.key(['Record', runId]);
   	datastore
@@ -67,6 +66,7 @@ app.listen(PORT, () => {
 app.post('/api/run/records', (req, res, next) => {
 const entities = []
 req.body.records.forEach( record => {
+record.timeStamp = Date.now()
 const recordKey = datastore.key("Record")
 entities.push(({key: recordKey, 
 data: record}))})
@@ -75,10 +75,10 @@ data: record}))})
 res
     .status(200)
     .set('Content-Type', 'application/json')
-    .send({hello: "world"})
+    .send(req.body.records)
 })
 
-app.get('/api/run/userName/:userName', (req, res, next) => {
+app.get('/api/run/:userName', (req, res, next) => {
  const userName = req.params.userName
  const query = datastore
 			.createQuery('Record')
@@ -89,8 +89,8 @@ app.get('/api/run/userName/:userName', (req, res, next) => {
 					const record = records[0]
 					const timeS = records[0].timeStamp
 					//const date = timeS.getDate()
-					const date = Date.prototype.getTime()
-					console.log("date: " , date)
+					//const date = Date.prototype.getTime()
+					//console.log("date: " , date)
 					res
   					   .status(200)
   					   .set('Content-Type', 'application/json')
@@ -100,7 +100,7 @@ app.get('/api/run/userName/:userName', (req, res, next) => {
 
 })
 
-app.get('/api/run/userNameAndPos/:userName/:position', (req, res, next) => {
+app.get('/api/run/:userName/:position', (req, res, next) => {
  const userName = req.params.userName
  const position = req.params.position
  const query = datastore
@@ -119,4 +119,35 @@ app.get('/api/run/userNameAndPos/:userName/:position', (req, res, next) => {
   					   .send(record) })
  
                         .catch(err => {console.error('ERROR:', err)})
+})
+
+app.get('/api/run/:timeStamps', (req, res, next) => {
+ const timeStamps = req.params.timeStamps.split(',').map(v => parseInt(v)).filter(v => !isNaN(v))
+ const timeStampMin = 0
+ const timeStampMax = 0
+/*
+//console.log(timeStamp[0])
+  if(timeStamps[0] >= timeStamps[1]) {
+	timeStampMax = timeStamp[0]
+	timeStampMin = timeStamp[1]
+}else {
+	timeStampMax = timeStamp[1]
+        timeStampMin = timeStamp[0]
+}
+ 
+const query = datastore
+                        .createQuery('Record')
+                        .filter('timeStamp', '>', timeStampMin)
+                        .filter('timeStamp', '<', timeStampMax)
+
+ datastore.runQuery(query)
+                        .then(results => {
+                                        const records = results[0]*/
+                                        res
+                                           .status(200)
+                                           .set('Content-Type', 'application/json')
+                                           .send(records)/*})
+ 
+                        .catch(err => {console.error('ERROR:', err)})
+*/
 })
